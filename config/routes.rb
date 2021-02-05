583efa1f8ad6d74941eb4fea7bd92c39418e360d
github username <github email address>
2021-02-05 11:06:33 -0800

    set up signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :regions
+  
+  get '/signup' => 'users#new'
+  post 'signup' => 'users#create'
+
   resources :comments
   resources :users
   resources :wineries
