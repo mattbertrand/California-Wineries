67182d95f56000450f8ce5f1aa5fcba4606fd24e
github username <github email address>
2021-02-05 10:05:15 -0800

    added region resource using rails generator

---
+++ app/models/region.rb
@@ -0,0 +1,4 @@
+class Region < ApplicationRecord
+  belongs_to :user
+  belongs_to :winery
+end
---
+++ db/migrate/20210205180456_create_regions.rb
@@ -0,0 +1,11 @@
+class CreateRegions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :regions do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+      t.references :winery, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
