6101b8062b24caa92d45f5a182fa70b1457aa7b6
github username <github email address>
2021-02-06 13:15:20 -0800

    added region controller index show

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,2 +1,12 @@
 class RegionsController < ApplicationController
+    def index
+        @regions = Region.all.includes(:wineries)
+    end
+
+    def show
+        @region = Region.find_by_id(params[:id])
+    end
+
+    def new
+    end
 end
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -3,7 +3,11 @@ class Winery < ApplicationRecord
   belongs_to :region
   has_many :comments
   has_many :users, through: :comments
+  
+  scope :alpha, -> { order(:title) }
+  
 end
+
 
 def region_attributes=(attr)
   self.region = Region.find_or_create_by(attr) if !attr[:name].blank?
