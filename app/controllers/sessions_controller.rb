563482af414f36f2f00d3246757855ec6aeaf1c1
github username <github email address>
2021-02-05 17:49:31 -0800

    added login page and methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,17 @@ class SessionsController < ApplicationController
     def home
     end
 
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect login info, please try again"
+            redirect_to '/login'
+        end
+    end
+
     def destroy
         session.clear
         redirect_to root_path
