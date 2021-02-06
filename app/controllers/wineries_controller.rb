b8ce2d0a74911d87178558247728648d11ad3dd1
github username <github email address>
2021-02-05 19:44:42 -0800

    added index page

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -1,3 +1,25 @@
 class WineriesController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new
+        @winery = Winery.new
+    end
+
+    def create
+        @winery = current_user.wineries.build(winery_params)
+        if @winery.save
+            redirect_to wineries_path
+        else
+            render :'new'
+        end
+    end
+
+    def index
+        @wineries = Winery.all
+    end
+
+    private
+        def winery_params
+            params.require(:winery).permit(:name, :website, :phone, :description)
+        end
 end
