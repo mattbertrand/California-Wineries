6a81ab7d9c2a5f120a201f7765ab1edfd63858a0
github username <github email address>
2021-02-07 14:56:49 -0800

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :users do
     resources :wineries, only: [:new, :create, :index]
   end
+  resources :region, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
