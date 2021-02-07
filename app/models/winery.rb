aefc8f697c91eca2d691e2afdc2843c7dacc07f8
github username <github email address>
2021-02-07 09:32:20 -0800

    updated wineries views

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -3,6 +3,8 @@ class Winery < ApplicationRecord
   belongs_to :region
   has_many :comments
   has_many :users, through: :comments
+
+  accepts_nested_attributes_for :region, reject_if: :all_blank
   
   scope :alpha, -> { order(:name) }
 
